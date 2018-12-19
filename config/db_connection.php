f9952d95d4e56df8066a487ea76b15ad65a29223
alexandrebsilva <alexandre.bsilva55@gmail.com>
2018-12-18 23:33:50 -0200

    second commit with the connection file

--- config/db_connection.php
+++ config/db_connection.php
@@ -1,1 +1,20 @@
 <?php
+
+function dbConnect(){
+  //parameters for the db connection
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $db_name = "php7";
+  //connection itself
+  try {
+      $conn = new PDO("mysql:host=$servername;dbname=$db_name", $username, $password);
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $conn;
+      }
+  catch(PDOException $e)
+      {
+      echo "Connection failed: " . $e->getMessage();
+      }
+}
--- index.php
+++ index.php
@@ -1,2 +1,20 @@
 <?php
+include_once "config/db_connection.php";
+$conn = dbConnect();
+
+
+
  ?>
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <!--<pre>
+      <?php var_dump($conn); ?>
+
+</pre>-->
+  </body>
+</html>
